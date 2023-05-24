b28cd4a7bdf6342879eea3bec1e14b71f7681356
Dylan Harness <dylan@NailasMacStudio.attlocal.net>
2023-05-24 13:39:29 -0700

    .

---
+++ www/src/components/Cursor.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { MousePosition, getMousePosition } from "../utils/mouse";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../app/store";
+
+const StyledCursor = styled.div<{ $radius: number }>`
+    box-sizing: border-box;
+    border: solid 1px black;
+    width: ${props => props.$radius}px;
+    height: ${props => props.$radius}px;
+    border-radius: ${props => props.$radius}px;
+    position: fixed;
+    pointer-events: none; 
+`;
+interface CursorProps {
+    active: boolean;
+}
+
+function Cursor({ active }: CursorProps) {
+    const brushSize = useSelector((state: RootState) => state.tools.brushSize);
+    const [mousePosition, setMousePosition] = useState<MousePosition>();
+
+    function trackMousePosition(event: MouseEvent) {
+        if (!document.body) return;
+        const p = getMousePosition(document.body, event);
+        setMousePosition(p);
+    }
+
+    useEffect(() => {
+        window.addEventListener("mousemove", trackMousePosition);
+        return () => {
+            window.removeEventListener("mousemove", trackMousePosition);
+        }
+    }, [])
+
+    if (!active || !mousePosition) return <></>;
+    const { x, y } = mousePosition;
+    const left = x - brushSize / 2;
+    const top = y - brushSize / 2;
+    const style = { left, top };
+
+    return (<StyledCursor style={style} $radius={brushSize} />);
+}
+
+export default Cursor;
--- www/src/features/toolsSlice.tsx
+++ www/src/features/toolsSlice.tsx
@@ -8,10 +8,12 @@ export enum ToolId {
 
 export interface ToolState {
     currentTool: ToolId
+    brushSize: number
 }
 
 const initialState: ToolState = {
     currentTool: ToolId.Brush,
+    brushSize: 10,
 }
 
 export const toolsSlice = createSlice({
@@ -21,9 +23,15 @@ export const toolsSlice = createSlice({
         setTool: (state, action: PayloadAction<ToolId>) => {
             state.currentTool = action.payload;
         },
+        incrementBrushSize: (state) => {
+            state.brushSize++;
+        },
+        decrementBrushSize: (state) => {
+            state.brushSize--;
+        }
     },
 })
 
 
-export const { setTool } = toolsSlice.actions
+export const { setTool, incrementBrushSize, decrementBrushSize } = toolsSlice.actions
 export default toolsSlice.reducer
